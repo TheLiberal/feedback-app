d11a49fa8a124a8af54e0814c47ae4dc1f7ea1cc
Blaise Gulaj <bgulaj@gmail.com>
2023-08-27 20:44:47 +0200

    initial commit

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,23 @@
+import Card from "../components/shared/Card"
+import {Link} from 'react-router-dom';
+
+
+function AboutPage() {
+  return (
+    <Card>
+      <div className="about">
+        <h1>About this project</h1>
+        <p>This is a React app to leave feedback.</p>
+        <p>Version: 1.0</p>
+
+
+    <p>
+      <Link to="/">Go Back</Link>
+    </p>
+
+      </div>
+    </Card>
+  )
+}
+
+export default AboutPage
